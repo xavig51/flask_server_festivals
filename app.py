b97014c3b4197a89784d0ebcd6b267fcd54e3703
xavig51 <47212264+xavig51@users.noreply.github.com>
2019-03-07 19:46:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
+from flask import Flask,Blueprint,url_for
+from flask_restplus import Api
+from flask import request
+import json
+from pymongo import MongoClient
+from flask_restplus import Namespace, Resource,fields,reqparse
+from application.festivales.festivalesRest import api
+from flask_cors import CORS, cross_origin
 
-from flask import Flask
-app = Flask('hello-cloudbuild')
+app=Flask(__name__)
 
-@app.route('/')
-def hello():
-  return "primer despligue simple\n"
+app = Flask(__name__)
+blueprint = Blueprint('api', __name__, url_prefix='/application')
+api = Api(blueprint,
+	title='festivales... a way of life',
+	version='v0.1',
+	description='servidor api rest',
+	doc='/',
+	)
+app.register_blueprint(blueprint)
 
-if __name__ == '__main__':
-  app.run(host = '0.0.0.0', port = 8080)
+api.add_namespace("festivales/0.1","informacion de festivales")
 
+def normalizacion(cadena):
+	cadena=cadena.replace("\"","").replace("'","").replace(": ","\":\"").replace(", ","\",\"").replace("{","{\"") \
+				.replace("}","\"}")
+	return cadena.replace("http\":\"","http:").replace("https\":\"","https:")
+
+client = MongoClient('mongodb+srv://cluster0-lhfgs.gcp.mongodb.net/test',username='javig13' \
+	,password='Estudiantes15',connect=False)
+
+class consulta(Resource):
+	@cross_origin()
+	def get(self):
+		salida="["
+		festivales = client['festivales'].spain
+		i=0
+		for festival in festivales.find():
+			#parcial=str(festival).replace("'),",")\",").replace("ObjectId(\'","\"ObjectId(").replace("\'","\"")
+			parcial=normalizacion(str(festival))
+			print(str(festival))
+			if(i==0):
+				salida+=parcial
+			else:
+				salida=salida+","+parcial
+			i+=1
+
+		salida+="]"
+		return salida
+
+api.add_resource(consulta,'/consulta',endpoint="consulta festivales españoles")
