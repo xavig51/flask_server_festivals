5e9c780219911e9ea351c090c9ce618737896c6f
xavig51 <47212264+xavig51@users.noreply.github.com>
2019-03-09 19:46:52 +0100

    mejora test

--- test_app.py
+++ test_app.py
@@ -1,11 +1,13 @@
-
 import unittest
 from app import client
+from pymongo.mongo_client import MongoClient
 
 class TestApp(unittest.TestCase):
 
   def test_connection(self):
-    self.assertTrue(client)
+  	self.assertEquals(type(client), MongoClient)
+  	self.assertTrue(client)
+
 
 if __name__ == '__main__':
   unittest.main()
