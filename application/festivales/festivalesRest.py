b216b63c2e71cd919ecbac942a15935386c8ee5a
xavig51 <47212264+xavig51@users.noreply.github.com>
2019-03-10 09:58:26 +0100

    Delete festivalesRest.py

--- application/festivales/festivalesRest.py
+++
@@ -1,5 +0,0 @@
-from flask_restplus import Namespace
-api=Namespace("festivales/0.1","informacion de festivales")
-
-from application.festivales.consultaFestivalesSpain import consulta
-api.add_resource(consulta,'/consulta',endpoint="consulta festivales españoles")
